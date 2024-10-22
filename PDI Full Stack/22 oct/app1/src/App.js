6ff468ab68d2738ad779acca1c7aefbdb63322d8
silvyapatel <silvya.patel@outlook.com>
2024-10-22 11:54:43 +0000

    files

---
+++ PDI Full Stack/22 oct/app1/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function MyButton(){
+  return(
+    <button>Click me! </button>
+  )
+}
+
+function App() {
+  return (
+    <div>
+   <h1>Hello world!</h1>
+   <MyButton/>
+   </div>
+  )
+}
+
+export default App;
---
+++ PDI Full Stack/22 oct/app1/src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import Header from '../Components/Header';
+import Footer from '../Components/Footer';
+
+
+function Home() {
+  return (
+    <div>
+        <Header/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
