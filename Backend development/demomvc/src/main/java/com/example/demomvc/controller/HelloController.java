59be02b1c16bfe15f1f8d2e03daa3f201e9e8d03
silvyapatel <silvya.patel@outlook.com>
2024-11-05 04:49:22 +0000

    files

--- Backend development/demomvc/src/main/java/com/example/demomvc/controller/HelloController.java
+++ Backend development/demomvc/src/main/java/com/example/demomvc/controller/HelloController.java
@@ -1,13 +1,22 @@
 package com.example.demomvc.controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping("/")
-    public String helloWorld(){
-        return "Hello world spring MVC";
+//    @RequestMapping("/")
+//    public String helloWorld(){
+//        return "Hello world spring MVC";
+//    }
+    @Value("${user}")
+    private String user;
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello " + user + ", Welome to Spring sessions.";
     }
 
 }
---
+++ Backend development/productsApp/src/main/java/com/cloudthat/productsapp/entity/Product.java
@@ -0,0 +1,68 @@
+package com.cloudthat.productsapp.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String productName;
+    private String productDescription;
+    private double price;
+
+    public Product() {
+    }
+
+    public Product( String productName, String productDescription, double price) {
+        this.productName = productName;
+        this.productDescription = productDescription;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", productName='" + productName + '\'' +
+                ", productDescription='" + productDescription + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
